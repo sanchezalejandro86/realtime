4f8503ae08f9c1b1651d06e4f94e1bf20e2776de
jlavera <jlavera92@gmail.com>
2015-08-24 11:52:28 -0300

    sin stun servers

--- public/javascripts/index/index.js
+++ public/javascripts/index/index.js
@@ -5,20 +5,20 @@ var herokuOpts= {
     port: '',
     //path: '/peerjs',
     debug: 3,
-    secure: true,
+    secure: true/*,
     config: { 'iceServers': [
                 {url:'stun:stun.xten.com'},
                /* {
                     url: 'turn:numb.viagenie.ca',
                     credential: 'muazkh',
                     username: 'webrtc@live.com'
-                }*/
+                }
                 {
                     url: 'turn:192.158.29.39:3478?transport=tcp',
                     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
                     username: '28224511:1379330808'
                 }
-            ] }
+            ] }*/
 };
 
 function getNewPeer(){
